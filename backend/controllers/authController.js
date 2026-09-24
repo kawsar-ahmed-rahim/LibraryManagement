e846d7de3a11e24c9d3e23c8bed4fceafcfd6aab
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-24 18:22:54 +0600

    till now

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -188,3 +188,91 @@ export async function loginUser(req, res) {
     });
   }
 }
+
+// get current user profile
+export async function getProfile(req, res) {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log("Error fetching user profile:", error);
+    res.status(500).json({
+      message: "Error fetching user profile:",
+      error: error.message,
+    });
+  }
+}
+
+// update user profile
+
+export async function updateProfile(req, res) {
+  try {
+    const {
+      name,
+      email,
+      phone,
+      department,
+      stream,
+      semester,
+      academicYear,
+      rollNumber,
+    } = req.body;
+    const user = await User.findById(req.user.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    if (email) {
+      const normalizedEmail = email.trim().toLowerCase();
+      if (normalizedEmail !== user.email.toLowerCase()) {
+        if (user.role === "user") {
+          return res
+            .status(400)
+            .json({
+              message: "Students are not allowed to change their email address",
+            });
+        }
+        if (
+          await User.findOne({ email: normalizedEmail, _id: { $ne: user._id } })
+        ) {
+          return res.status(400).json({ message: "Email already in use" });
+        }
+        user.email = normalizedEmail;
+      }
+    }
+    if (phone) {
+      const cleanPhone = phone.toString().replace(/\D/g, "");
+      if (cleanPhone.length !== 10) {
+        return res
+          .status(400)
+          .json({ message: "Mobile number must be exactly 10 digits" });
+      }
+      user.phone = cleanPhone;
+    }
+
+    if (name) user.name = name;
+    if (department) user.department = department;
+    if (stream) user.stream = stream;
+    if (semester) user.semester = semester;
+    if (academicYear) user.year = academicYear;
+    if (rollNumber) user.rollNo = rollNumber;
+
+    await user.save();
+    res
+      .status(200)
+      .json({ success: true, message: "Profile updated successfully", user });
+  } catch (error) {
+    console.error("Error updating profile:", error);
+    res
+      .status(500)
+      .json({ message: "Error updating profile", error: error.message });
+  }
+}
+
+// to get all students account(admin)
+export async function getUsers(req, res){
+  try {
+    
+  } catch (error) {
+    
+  }
+}
